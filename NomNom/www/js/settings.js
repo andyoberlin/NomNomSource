2f9814dea116e2eefa39c239cd511dd550dc4ab0
malloyjw <malloyjw@miamioh.edu>
2013-04-25 19:10:50 -0700

    Made some toggle boxes for the settings page.  Not sure if it's the best way to create/handle them, though.  Learning!

--- NomNom/www/js/settings.js
+++ NomNom/www/js/settings.js
@@ -5,3 +5,11 @@ $('#home').on('click', function() {
 $('#settings').on('click', function() {
 	window.location = 'settings.html';
 });
+
+$('#option1-container').on('click', function() {
+	$('#option1-switch').toggle();
+});
+
+$('#option2-container').on('click', function() {
+	$('#option2-switch').toggle();
+});
